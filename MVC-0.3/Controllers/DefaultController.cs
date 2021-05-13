6ee93dc3922e39df28021856a5dacad2d2d597b3
fırat Yıldızhan <hanyildizfirat@hotmail.com>
2021-05-13 21:08:48 +0300

    Add project files.

---
+++ MVC-0.3/Controllers/DefaultController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVC_0._3.Controllers
+{
+    public class DefaultController : Controller
+    {
+        // GET: Default
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
